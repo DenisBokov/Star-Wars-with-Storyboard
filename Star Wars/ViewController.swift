91ea49a1666e09134e1ffe4b2b9513170a6fa773
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-10-19 12:39:00 +0300

    Add View

--- Star Wars/ViewController.swift
+++ Star Wars/ViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UICollectionViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
     }
-
-
+    
+    
+    
 }
 
